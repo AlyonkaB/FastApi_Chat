ed5e002be71801cb87547783dff57812c1588b13
Guacamole <macbook@MacBook-Air-MacBook.local>
2025-03-14 13:23:00 +0200

    added saving JWToken in cookies

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, HTTPException
-from fastapi.params import Depends
+from fastapi import APIRouter, HTTPException, Depends, Response
 from fastapi_jwt_auth import AuthJWT
 from fastapi_jwt_auth.exceptions import AuthJWTException
 from pydantic import BaseModel
@@ -20,16 +19,23 @@ def get_config():
 
 @router.post("/login")
 async def login(
-    user: UserLogin, Authorize: AuthJWT = Depends(), db: AsyncSession = Depends(get_db)
+    user: UserLogin, response: Response, Authorize: AuthJWT = Depends(), db: AsyncSession = Depends(get_db),
 ):
     try:
         db_user = await get_user_by_username(db, user.username)
         if not db_user or not verify_password(user.password, db_user.hashed_password):
             raise HTTPException(
-                status_code=400, detail="Невірне ім'я користувача або пароль"
+                status_code=400, detail="Incorrect username or password"
             )
 
         access_token = Authorize.create_access_token(subject=db_user.username)
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            secure=True,  # You may want to use this for HTTPS connections
+            samesite="Strict"  # Optional, helps prevent CSRF attacks
+        )
         return {"access_token": access_token}
     except AuthJWTException as e:
-        raise HTTPException(status_code=401, detail=f"Помилка JWT: {str(e)}")
+        raise HTTPException(status_code=401, detail=f"Error JWT: {str(e)}")
